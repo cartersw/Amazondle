be1801c794c50b97c04baeb35c2236d6ba73b1f3
Jhon Tabio <JhonTabioCS@gmail.com>
2024-03-04 17:48:18 -0500

    Expedited function calls and scrapper speed. Scraping item info is 2400% faster than before

--- backend/utils/amazonScraper.ts
+++ backend/utils/amazonScraper.ts
@@ -5,7 +5,7 @@ async function scrapeBestSellers(): Promise<string[]>
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto("https://www.amazon.com/gp/bestsellers/", { waitUntil: 'networkidle0' }) ;
+  await page.goto("https://www.amazon.com/gp/bestsellers/", { waitUntil: 'networkidle2' }) ;
 
   const itemLinks: string[] = await page.evaluate(() => {
     const links: string[] = [];
@@ -32,7 +32,7 @@ async function scrapeItemLink(link: string): Promise<string[]>
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto(link, { waitUntil: 'networkidle0', timeout: 0 }) ;
+  await page.goto(link, { waitUntil: 'networkidle2' });
 
   const itemLinks: string[] = await page.evaluate(() => {
     const title = document.querySelector("#productTitle")?.textContent?.trim() || "Title Not Found";
@@ -65,4 +65,3 @@ async function main()
 }
 
 main().catch(console.error);
-//scrapeItemLink("https://www.amazon.com/Quencher-FlowState-Stainless-Insulated-Smoothie/dp/B0BW2LD72T/ref=zg_bs_c_home-garden_d_sccl_1/141-9578934-4854767?pd_rd_w=KMUux&content-id=amzn1.sym.309d45c5-3eba-4f62-9bb2-0acdcf0662e7&pf_rd_p=309d45c5-3eba-4f62-9bb2-0acdcf0662e7&pf_rd_r=639YK1K4VMJQE2596309&pd_rd_wg=th1M8&pd_rd_r=b61c9941-4235-4491-83ef-9ac6a436a914&pd_rd_i=B0CRMZ7TFZ&th=1").catch(console.error);
