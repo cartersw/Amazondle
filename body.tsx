c6c73b915085c17935b8a693f73442475190f067
yahy4 <98236402+yahy4@users.noreply.github.com>
2024-04-15 20:14:02 -0400

    Add files via upload
    Functional Frontend with Clerk implementation. Created separate TSX files for body, heading etc

---
+++ body.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { SignedIn, SignedOut, UserButton, useUser, useClerk } from '@clerk/clerk-react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+
+function body(){
+    return (
+        <div>
+        <p>fuyihiufiufiufhiufihufuifuifuih</p>
+     </div>
+    );
+  }
+  
+  export default body;
