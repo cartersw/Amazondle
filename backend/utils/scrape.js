23e90c65c4c7d91a298901993bf8476ba35a8696
Jhon Tabio <JhonTabioCS@gmail.com>
2024-04-17 16:53:13 -0400

    Added fallback to when the first scrape doesnt work. This counteracts Amazon's multiple ways to display results

--- backend/utils/scrape.js
+++ backend/utils/scrape.js
@@ -20,15 +20,18 @@ const scrape = (html) => {
   const root = parse(html);
 
   // Gather product titles
-  const titleNodes = root.querySelectorAll("div.a-section.a-spacing-base h2 span.a-color-base.a-text-normal");
+  let titleNodes = root.querySelectorAll("div.a-section.a-spacing-base h2 span.a-color-base.a-text-normal");
+  titleNodes = titleNodes.length == 0 ? root.querySelectorAll("div.a-section.a-spacing-small span.a-size-medium.a-color-base.a-text-normal") : titleNodes;
   const titles = titleNodes.map(node => node.innerText);
 
   // Gather prices
-  const priceNodes = root.querySelectorAll('div.a-section.a-spacing-base span.a-price[data-a-color="base"] span.a-offscreen');
+  let priceNodes = root.querySelectorAll('div.a-section.a-spacing-base span.a-price[data-a-color="base"] span.a-offscreen');
+  priceNodes = priceNodes.length == 0 ? root.querySelectorAll("div.a-section span.a-price span.a-offscreen") : priceNodes;
   const prices = priceNodes.map(node => node.innerText);
 
   // Gather pictures
-  const pictureNodes = root.querySelectorAll("div.a-section.a-spacing-base img.s-image[srcset]");
+  let pictureNodes = root.querySelectorAll("div.a-section.a-spacing-base img.s-image[srcset]");
+  pictureNodes = pictureNodes.length == 0 ? root.querySelectorAll("div.a-section.aok-relative.s-image-fixed-height img") : pictureNodes;
   const pictures = pictureNodes.map(node => node.getAttribute("src"));
 
   // Put data together
@@ -51,7 +54,7 @@ const scrapeSearch = async (item) => {
 };
 
 const main = async () => {
-  const result = await scrapeSearch("Pencil Case");
+  const result = await scrapeSearch("Laptop");
   console.log(result);
 }
 
