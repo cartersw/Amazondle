df018592691dab72be679bd2d05a42fc6eff11bb
gabeparra <ma.parrar16@gmail.com>
2024-04-23 20:57:26 -0400

    mobile stuff

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-    name: 'MyApp',
+    name: 'Amazondle',
     version: '1.0.0',
-    scheme: "myapp",
+    scheme: "Amazondle",
+    android: {
+        package: 'com.amazondle.amazondle',
+    },
     extra: {
         clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+        "eas": {
+            "projectId": "c1d68488-4df4-47cb-932c-a0de949952d4"
+        }
     },
 };
